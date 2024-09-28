f06cca1cd2bc589329e8fa15d9c10addfe87426c
quurkyhongyu <74701224+qiuji10@users.noreply.github.com>
2024-09-28 13:02:16 +0800

    implement weapon system

---
+++ Assets/1_Scripts/Weapon System/Bullet.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed = 5;
+    public float maxDistance = 5f;
+    Rigidbody2D _rb;
+
+    private Vector2 origin;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        origin = transform.position;
+    }
+
+    public void SetDirection(Quaternion rotation)
+    {
+        transform.rotation = rotation;
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = speed * Time.fixedDeltaTime * transform.right;
+
+        if (Vector2.Distance(origin, transform.position) >= maxDistance)
+            Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.CompareTag("Player"))
+        {
+            if (col.gameObject.TryGetComponent(out IHealth healthComponent))
+            {
+                healthComponent.Damage(damage);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/1_Scripts/Weapon System/Gun.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour, IWeapon
+{
+    [Header("Settings'")]
+    public float cd;
+    public Transform firePoint;
+    public Bullet bulletPrefab;
+
+    [Header("Debug")]
+    [SerializeField] private Transform _target;
+
+    private float cdTimer;
+
+    public Dictionary<string, object> attributes { get; set; }
+
+    private void Update()
+    {
+        if (cdTimer > 0)
+        {
+            cdTimer -= Time.deltaTime;
+        }
+    }
+
+    [NaughtyAttributes.Button]
+    public void Attack()
+    {
+        cdTimer = cd;
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+
+    public bool CanAttack()
+    {
+        return cdTimer <= 0;
+    }
+}
