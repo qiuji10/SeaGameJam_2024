4f516cfb26d2fa7a1830aa2d982788d96ec48d65
quurkyhongyu <74701224+qiuji10@users.noreply.github.com>
2024-09-28 19:20:52 +0800

    update prefab

--- Assets/1_Scripts/AI/AIBlackboard.cs
+++ Assets/1_Scripts/AI/AIBlackboard.cs
@@ -7,21 +7,20 @@ public class AIBlackboard : MonoBehaviour
     [Header("Config Variables")]
     public float baseSpeed = 4;
     public float chaseSpeed = 8;
+    public List<Transform> waypoints;
 
     [Header("Runtime Variables")]
     public float speed;
+    public int destinationIndex;
     public EDirection direction;
 
-    public List<Transform> waypoints;
-    public int destinationIndex;
-
+    [Header("References")]
     public Animator animator;
     public Rigidbody2D rb;
     public AIHealth health;
     public AITransform agentTransform;
     public ConeDetection alertDetection;
     public ConeDetection attackDetection;
-
     public Transform target;
     public GameObject weaponGameObject;
     public IWeapon weapon;
